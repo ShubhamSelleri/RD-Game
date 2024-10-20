503ebb73fc29e170c6bf1668b0533d24875461e8
stijnavaux <147623296+stijnavaux@users.noreply.github.com>
2024-10-20 16:48:00 +0200

    wiitest

--- Assets/Scripts/Platformer/mouseLight.cs
+++ Assets/Scripts/Platformer/mouseLight.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WiimoteApi;
 
 public class MouseLight : MonoBehaviour
 {
+    Wiimote mote;
 
+    void Start(){
+        WiimoteManager.FindWiimotes();
+        mote = WiimoteManager.Wiimotes[0];
+        mote.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
+        mote.Accel.CalibrateAccel(AccelCalibrationStep.A_BUTTON_UP);
+        mote.SendPlayerLED(true, false, false, false);
+    }
 
     void Update()
     {
+
+        if(mote.Button.home){
+            mote.Accel.CalibrateAccel(AccelCalibrationStep.A_BUTTON_UP);
+        }
+        /*
         // Get the mouse position in screen coordinates
         Vector3 mousePosition = Input.mousePosition;
 
@@ -17,5 +31,20 @@ public class MouseLight : MonoBehaviour
 
         // Set the position of the GameObject to the mouse position
         transform.position = mousePosition;
+        */
+        if(mote != null){
+            float[] acell = mote.Accel.GetCalibratedAccelData();
+
+            mousePosition.x = acell[0]- 0.3f;
+            mousePosition.y = -acell[1] + 0.3f;
+
+            if(mousePosition.x > -0.3f && mousePosition.x < 0.3f) mousePosition.x = 0;
+            if(mousePosition.y > -0.3f && mousePosition.y < 0.3f) mousePosition.y = 0;
+        }
     }
+/*
+    IEnumerator activateMote(){
+        yield return new WaitUntil(() => WiimoteManager.HasWiimote());
+        mote = WiimoteManager.Wiimotes[0];
+    }*/
 }
