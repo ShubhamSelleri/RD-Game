24b046936beb5965550b9bf68f6fd4f32e903bb1
stijnavaux <147623296+stijnavaux@users.noreply.github.com>
2024-10-20 17:37:09 +0200

    wiitest

--- Assets/Scripts/Platformer/mouseLight.cs
+++ Assets/Scripts/Platformer/mouseLight.cs
@@ -26,7 +26,7 @@ public class MouseLight : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
         
         // Convert screen position to world position
-        //mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mousePosition.z = 0; // Set z to 0 for 2D (if in 3D, adjust accordingly)
 
         // Set the position of the GameObject to the mouse position
