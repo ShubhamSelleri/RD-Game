46103bf10bdbb8bf28ad981368ceb90113262eae
Lolly753 <101336522+Lolly753@users.noreply.github.com>
2024-11-28 15:14:52 +0100

    gear mulitAxis
    you can now choose what axis the gear rotates around

--- Assets/Scripts/GearController/gearRotate.cs
+++ Assets/Scripts/GearController/gearRotate.cs
@@ -4,12 +4,13 @@ public class gearRotate : MonoBehaviour
 {
     // The target object we want to rotate (can be set in the Inspector)
     public GameObject targetObject;
+    public string rotationAxis = "X";
 
     // Maximum value the rotary encoder can read
     private float maxEncoderValue = 60f;
 
     // Minimum and maximum rotation angles
-    private float minRotation = -180f;  // Minimum angle (start position)
+    private float minRotation = 0f;  // Minimum angle (start position)
     private float maxRotation = 180f; // Maximum angle (full rotation)
 
     // This will hold the current angle
@@ -17,9 +18,7 @@ public class gearRotate : MonoBehaviour
     private int lastPosition = 1;
     private int step = 0;
 
-
     // Invoked when a line of data is received from the serial device.
-
     void OnMessageArrived(string msg)
     {
         // Parse the message to an integer (we expect values between 1 and 60)
@@ -27,15 +26,30 @@ public class gearRotate : MonoBehaviour
         {
             step = position - lastPosition;
             Debug.Log("step = " + step + " , position = " + position);
+
             // Map the encoder value to a rotation angle (from 0 to 360 degrees)
-            currentRotation = Mathf.Lerp(minRotation, maxRotation, ((step + 60) % 60) / (maxEncoderValue));
+            currentRotation = Mathf.Lerp(minRotation, maxRotation, ((step + maxEncoderValue) % maxEncoderValue) / maxEncoderValue);
 
             // Rotate the target object based on the mapped value
             if (targetObject != null)
             {
-                targetObject.transform.Rotate(0f, currentRotation, 0f, Space.Self);
-
+                switch (rotationAxis)
+                {
+                    case "x": // Rotate around X-axis
+                        targetObject.transform.Rotate(currentRotation, 0f, 0f, Space.Self);
+                        break;
+                    case "y": // Rotate around Y-axis
+                        targetObject.transform.Rotate(0f, currentRotation, 0f, Space.Self);
+                        break;
+                    case "z": // Rotate around Z-axis
+                        targetObject.transform.Rotate(0f, 0f, currentRotation, Space.Self);
+                        break;
+                    default:
+                        Debug.LogWarning("Invalid rotation axis specified. Use 'x', 'y', or 'z'.");
+                        break;
+                }
             }
+
             lastPosition = position;
         }
     }
