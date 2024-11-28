e135af9a0d616f373f16bdae6be221410a226056
Lolly753 <101336522+Lolly753@users.noreply.github.com>
2024-11-28 14:57:58 +0100

    gear rotates in scene
    gear rotates in scene again, but around wrong axis

--- Assets/Scripts/GearController/gearRotate.cs
+++ Assets/Scripts/GearController/gearRotate.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GearRotate : MonoBehaviour
+public class gearRotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // The target object we want to rotate (can be set in the Inspector)
+    public GameObject targetObject;
+
+    // Maximum value the rotary encoder can read
+    private float maxEncoderValue = 60f;
+
+    // Minimum and maximum rotation angles
+    private float minRotation = -180f;  // Minimum angle (start position)
+    private float maxRotation = 180f; // Maximum angle (full rotation)
+
+    // This will hold the current angle
+    private float currentRotation = 0f;
+    private int lastPosition = 1;
+    private int step = 0;
+
+
+    // Invoked when a line of data is received from the serial device.
+
+    void OnMessageArrived(string msg)
     {
-        
+        // Parse the message to an integer (we expect values between 1 and 60)
+        if (int.TryParse(msg, out int position))
+        {
+            step = position - lastPosition;
+            Debug.Log("step = " + step + " , position = " + position);
+            // Map the encoder value to a rotation angle (from 0 to 360 degrees)
+            currentRotation = Mathf.Lerp(minRotation, maxRotation, ((step + 60) % 60) / (maxEncoderValue));
+
+            // Rotate the target object based on the mapped value
+            if (targetObject != null)
+            {
+                targetObject.transform.Rotate(0f, currentRotation, 0f, Space.Self);
+
+            }
+            lastPosition = position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
     {
-        
+        if (success)
+        {
+            Debug.Log("Successfully connected to the gear input device.");
+        }
+        else
+        {
+            Debug.Log("Failed to connect or disconnected from the gear input device.");
+        }
     }
 }
