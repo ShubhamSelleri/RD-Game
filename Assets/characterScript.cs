39568304f3d86ed40a5f1b7f1deb00ba5d04a3b7
Lolly753 <101336522+Lolly753@users.noreply.github.com>
2024-12-05 15:08:18 +0100

    Character moves horizontally
    very shitty no animations

--- Assets/characterScript.cs
+++ Assets/characterScript.cs
@@ -5,19 +5,33 @@ using UnityEngine.InputSystem;
 
 
 
-
 public class characterScript : MonoBehaviour
 {
 
     private PlayerInput playerInput;
+    private CharacterController characterController;
+    private Vector2 currentMovementInput;
+    private Vector3 currentMovement;
+
+    private bool isMovementPressed;
 
     private void Awake()
     {
         playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
 
-        playerInput.CharacterControls.Move.started += context => { Debug.Log(context.ReadValue<Vector2>()); };
+        playerInput.CharacterControls.Move.started += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        playerInput.CharacterControls.Move.performed += onMovementInput;
     }
 
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        isMovementPressed = currentMovementInput.x != 0;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +43,7 @@ public class characterScript : MonoBehaviour
 
     void Update()
     {
-
+        characterController.Move(currentMovement * Time.deltaTime);
     }
 
     void OnEnable()
