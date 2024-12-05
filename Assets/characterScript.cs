58473b23bbaebe42746e67f6001a6fbe7175e5a0
Lolly753 <101336522+Lolly753@users.noreply.github.com>
2024-12-05 23:08:45 +0100

    Added player V2

--- Assets/characterScript.cs
+++ Assets/characterScript.cs
@@ -10,8 +10,16 @@ public class characterScript : MonoBehaviour
 
     public float rotationFactorPerFrame = 15.0f;
     public float moveSpeed = 10f;
-    public float gravity = 9.81f;
     public float maxVerticalSpeed = 10f;
+    public float fallMultiplier = 2.0f;
+
+
+    public float maxJumpHeight = 1.0f;
+    public float maxJumpTime = 0.5f;
+    private float initialJumpVelocity;
+
+    private bool isJumping = false;
+    private float gravity;
 
     private PlayerInput playerInput;
     private CharacterController characterController;
@@ -21,12 +29,16 @@ public class characterScript : MonoBehaviour
     private int JumpHash;
     private int FallingHash;
 
+    private bool isJumpAnimating;
+    private bool isFallingAnimating;
+
     private Vector2 currentMovementInput;
     private Vector3 currentMovement;
 
     private bool isMovementPressed;
-    private bool isGravityInverted;
+    private bool isGravityInverted = false;
     private bool isJumpPressed = false;
+
 
     private void Awake()
     {
@@ -47,25 +59,12 @@ public class characterScript : MonoBehaviour
 
         playerInput.CharacterControls.Jump.started += onJump;
         playerInput.CharacterControls.Jump.canceled += onJump;
+
+        setupJumpVariables();
+        maxVerticalSpeed = Mathf.Max(initialJumpVelocity,maxVerticalSpeed);
     }
 
-    void onJump(InputAction.CallbackContext context)
-    {
-        isJumpPressed = context.ReadValueAsButton();
-        Debug.Log("isJumpPressed:  " + isJumpPressed);
-        if (isJumpPressed && characterController.isGrounded)
-        {
-
-        }
-    }
-
-
-    void onMovementInput(InputAction.CallbackContext context)
-    {
-        currentMovementInput = context.ReadValue<Vector2>();
-        currentMovement.x = currentMovementInput.x;
-        isMovementPressed = currentMovementInput.x != 0;
-    }
+    
 
     // Start is called before the first frame update
     void Start()
@@ -77,64 +76,152 @@ public class characterScript : MonoBehaviour
 
     void Update()
     {
-        handleGravity();
-        characterController.Move(currentMovement * moveSpeed * Time.deltaTime);
         handleAnimation();
         handleRotation();
 
+        characterController.Move(currentMovement * Time.deltaTime);
+
+        handleGravity();
+        handleMaxVerticalSpeed();
+        handleJump();
+
+    }
+
+    void onJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
+    }
+
+    void handleJump()
+    {
+        if (!isJumping && characterController.isGrounded && isJumpPressed)
+        {
+            animator.SetBool(JumpHash, true);
+            isJumpAnimating = true;
+            isJumping = true;
+            currentMovement.y = initialJumpVelocity * 0.5f; //asumes initial y velocity is 0;
+        }
+        else if(!isJumpPressed && isJumping && characterController.isGrounded)
+        {
+            isJumping = false;
+        }
+    }
+
+    void setupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (2 * maxJumpHeight) / Mathf.Pow(timeToApex,2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+
+        Debug.Log("timeToApex = " + timeToApex);
+        Debug.Log("gravity = " + gravity);
+        Debug.Log("initialJumpVelocity = " + initialJumpVelocity);
+    }
+
+    void onMovementInput(InputAction.CallbackContext context)
+    {
+        currentMovementInput = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x * moveSpeed;
+        isMovementPressed = currentMovementInput.x != 0;
     }
 
     void handleAnimation()
     {
-        bool isRunning = animator.GetBool(RunHash);
-        bool isJumping = animator.GetBool(JumpHash);
-        bool isFalling = animator.GetBool(FallingHash);
+        bool isAnimatorRunning = animator.GetBool(RunHash);
+        bool isAnimatorJumping = animator.GetBool(JumpHash);
+        bool isAnimatorFalling = animator.GetBool(FallingHash);
 
-        if (isMovementPressed && !isRunning)
+        if (isMovementPressed && !isAnimatorRunning)
         {
             animator.SetBool(RunHash, true);
         }
-        else if (!isMovementPressed && isRunning)
+        else if (!isMovementPressed && isAnimatorRunning)
         {
             animator.SetBool(RunHash, false);
         }
     }
 
+    void handleMaxVerticalSpeed()
+    {
+        bool isMaxVerticalSpeedExceeded = Mathf.Abs(currentMovement.y) > maxVerticalSpeed;
+        if (isMaxVerticalSpeedExceeded)
+        {
+            if (isGravityInverted)
+            {
+                currentMovement.y = maxVerticalSpeed;
+            }
+            else
+            {
+                currentMovement.y = -maxVerticalSpeed;
+            }
+        }
+    }
+
     void handleGravity()
     {
+        //checks Y velocity depending on gravity and if jump is released
+        bool isFalling = (isGravityInverted ? currentMovement.y >= 0.0f : currentMovement.y < -0.05f);
+
+        if(isFalling && !isFallingAnimating && !characterController.isGrounded)
+        {
+            animator.SetBool(FallingHash, true);
+            isFallingAnimating = true;
+        }
+        else if(characterController.isGrounded && isFallingAnimating )
+        {
+            animator.SetBool(FallingHash, false);
+            isFallingAnimating = false;
+        }
+        
         if(characterController.isGrounded)
         {
             float groundedGravity = -.05f;
             currentMovement.y = groundedGravity;
+            animator.SetBool(JumpHash, false);
+            isJumpAnimating = false;
+
         }
-        else
+        else if (isFalling)
         {
+            animator.SetBool(FallingHash, true);
+            animator.SetBool(JumpHash, false);
+            isJumpAnimating = false;
+
+            float previousYVelocity = currentMovement.y;
+            float newYVelocity;
+            float nextYVelocity;
             //change sign of gravity if needed
-
-            bool isMaxVerticalSpeedExceeded = isGravityInverted ? (currentMovement.y - gravity * Time.deltaTime) > maxVerticalSpeed :
-                                                                  (currentMovement.y + gravity * Time.deltaTime) < maxVerticalSpeed ;
-
             if (isGravityInverted)
             {
-                if (isMaxVerticalSpeedExceeded)
-                {
-                    currentMovement.y = maxVerticalSpeed;
-                }
-                else
-                {
-                    currentMovement.y += gravity * Time.deltaTime;
-                }
+                newYVelocity = currentMovement.y + (gravity * fallMultiplier* Time.deltaTime);
+                nextYVelocity = (currentMovement.y + newYVelocity) * 0.5f;
+                currentMovement.y = nextYVelocity;
             }
             else
             {
-                if (isMaxVerticalSpeedExceeded)
-                {
-                    currentMovement.y = -maxVerticalSpeed;
-                }
-                else
-                {
-                    currentMovement.y -= gravity * Time.deltaTime;
-                }
+                newYVelocity = currentMovement.y - (gravity * fallMultiplier * Time.deltaTime);
+                nextYVelocity = (currentMovement.y + newYVelocity) * 0.5f;
+                currentMovement.y = nextYVelocity;
+            }
+        }
+        else
+        {
+
+            float previousYVelocity = currentMovement.y;
+            float newYVelocity;
+            float nextYVelocity;
+            //change sign of gravity if needed
+            if (isGravityInverted)
+            {
+                newYVelocity = currentMovement.y + (gravity * Time.deltaTime);
+                nextYVelocity = (currentMovement.y + newYVelocity) * 0.5f;
+                currentMovement.y = nextYVelocity;
+            }
+            else
+            {
+                newYVelocity = currentMovement.y - (gravity * Time.deltaTime);
+                nextYVelocity = (currentMovement.y + newYVelocity) * 0.5f;
+                currentMovement.y = nextYVelocity;
             }
         }
     }
