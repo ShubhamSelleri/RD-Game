c791912bbb5e98ce2fe76e4d444e619929e74a1e
Lolly753 <101336522+Lolly753@users.noreply.github.com>
2024-12-05 19:25:31 +0100

    Gravity added
    oh yeah baby

--- Assets/characterScript.cs
+++ Assets/characterScript.cs
@@ -10,27 +10,49 @@ public class characterScript : MonoBehaviour
 
     public float rotationFactorPerFrame = 15.0f;
     public float moveSpeed = 10f;
+    public float gravity = 9.81f;
+    public float maxVerticalSpeed = 10f;
 
     private PlayerInput playerInput;
     private CharacterController characterController;
     private Animator animator;
 
+    private int RunHash;
+    private int JumpHash;
+    private int FallingHash;
 
     private Vector2 currentMovementInput;
     private Vector3 currentMovement;
 
     private bool isMovementPressed;
+    private bool isGravityInverted;
 
     private void Awake()
     {
+
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+
+        RunHash = Animator.StringToHash("Run");
+        JumpHash = Animator.StringToHash("Jump");
+        FallingHash = Animator.StringToHash("Falling");
 
 
         playerInput.CharacterControls.Move.started += onMovementInput;
         playerInput.CharacterControls.Move.canceled += onMovementInput;
         playerInput.CharacterControls.Move.performed += onMovementInput;
+        playerInput.CharacterControls.Jump.started += onJump;
+        playerInput.CharacterControls.Jump.canceled += onJump;
+    }
+
+    void onJump(InputAction.CallbackContext context)
+    {
+        bool isJumpPressed = context.ReadValueAsButton();
+        if (isJumpPressed && characterController.isGrounded)
+        {
+
+        }
     }
 
 
@@ -44,13 +66,14 @@ public class characterScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
 
     void Update()
     {
+        handleGravity();
         characterController.Move(currentMovement * moveSpeed * Time.deltaTime);
         handleAnimation();
         handleRotation();
@@ -59,17 +82,56 @@ public class characterScript : MonoBehaviour
 
     void handleAnimation()
     {
-        bool isRunning = animator.GetBool("Run");
-        bool isJumping = animator.GetBool("Jump");
-        bool isFalling = animator.GetBool("Falling");
+        bool isRunning = animator.GetBool(RunHash);
+        bool isJumping = animator.GetBool(JumpHash);
+        bool isFalling = animator.GetBool(FallingHash);
 
         if (isMovementPressed && !isRunning)
         {
-            animator.SetBool("Run", true);
+            animator.SetBool(RunHash, true);
         }
-        else if ( !isMovementPressed && isRunning)
+        else if (!isMovementPressed && isRunning)
         {
-            animator.SetBool("Run", false);
+            animator.SetBool(RunHash, false);
+        }
+    }
+
+    void handleGravity()
+    {
+        if(characterController.isGrounded)
+        {
+            float groundedGravity = -.05f;
+            currentMovement.y = groundedGravity;
+        }
+        else
+        {
+            //change sign of gravity if needed
+
+            bool isMaxVerticalSpeedExceeded = isGravityInverted ? (currentMovement.y - gravity * Time.deltaTime) > maxVerticalSpeed :
+                                                                  (currentMovement.y + gravity * Time.deltaTime) < maxVerticalSpeed ;
+
+            if (isGravityInverted)
+            {
+                if (isMaxVerticalSpeedExceeded)
+                {
+                    currentMovement.y = maxVerticalSpeed;
+                }
+                else
+                {
+                    currentMovement.y += gravity * Time.deltaTime;
+                }
+            }
+            else
+            {
+                if (isMaxVerticalSpeedExceeded)
+                {
+                    currentMovement.y = -maxVerticalSpeed;
+                }
+                else
+                {
+                    currentMovement.y -= gravity * Time.deltaTime;
+                }
+            }
         }
     }
 
